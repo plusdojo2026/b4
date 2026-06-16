45778b66f3cdcf63d819a7f864acfac29ba46029
plusdojo2026-satoaoi <sato-aoi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 16:01:27 +0900

    Create UserSetting.java

---
+++ source/src/main/java/dto/UserSetting.java
@@ -0,0 +1,32 @@
+package dto;
+
+import java.io.Serializable;
+
+/**
+ * ユーザー設定データ受け渡し
+ */
+public class UserSetting implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // 保持するデータ
+    private String userName;
+    private int childCount;
+    private String garbageName;
+    private String garbageDay;
+
+    // コンストラクタ
+    public UserSetting() {}
+
+    // ゲッターとセッター
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
+
+    public int getChildCount() { return childCount; }
+    public void setChildCount(int childCount) { this.childCount = childCount; }
+
+    public String getGarbageName() { return garbageName; }
+    public void setGarbageName(String garbageName) { this.garbageName = garbageName; }
+
+    public String getGarbageDay() { return garbageDay; }
+    public void setGarbageDay(String garbageDay) { this.garbageDay = garbageDay; }
+}
