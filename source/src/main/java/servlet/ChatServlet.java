ee684e981f3ee6bf25457cf5de4d6e9df840d108
plusdojo2026-morita <morita-sae-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 17:03:56 +0900

    Update ChatServlet.java

--- source/src/main/java/servlet/ChatServlet.java
+++ source/src/main/java/servlet/ChatServlet.java
@@ -1,6 +1,17 @@
 package servlet;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dto.Message;
 
 
 
@@ -9,5 +20,38 @@ import javax.servlet.http.HttpServlet;
  */
 @WebServlet("/ChatServlet")
 public class ChatServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+//		HttpSession session = request.getSession();
+//		if (session.getAttribute("id") == null) {
+//			response.sendRedirect("/LoginServlet");
+//			return;
+//		}
+
+
+		//チャット履歴を保存するためのリスト
+		List<Message> messageList = new ArrayList<Message>();
+		
+		//最初の画面
+		messageList.add(new Message("bot", "お疲れ様！何か家事やった？"));
+		
+		//セッションを取得		
+		HttpSession session = request.getSession();
+		
+		//チャット履歴を保存
+		session.setAttribute("messageList", messageList);
+		
+		// チャットページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/chat.jsp");
+		dispatcher.forward(request, response);
+		
+
+    }
+		
 }
