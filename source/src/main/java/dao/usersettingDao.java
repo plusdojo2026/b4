3e1ab74c8621118641a97be88f7800f5e98e0a7e
plusdojo2026-satoaoi <sato-aoi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-24 17:23:54 +0900

    Update usersettingDao.java

--- source/src/main/java/dao/usersettingDao.java
+++ source/src/main/java/dao/usersettingDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import dto.UserSetting;
 
@@ -64,4 +65,63 @@ public class usersettingDao {
         } catch (Exception e) { e.printStackTrace(); }
         return user;
     }
+    
+    
+    //アイコン追加用メソッド
+    public boolean update(int icon_id,int user_id) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b4?"
+			        + "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true&allowPublicKeyRetrieval=true", // ★末尾に「&allowPublicKeyRetrieval=true」を追記
+			        "root", "Aoisql");
+
+			// SQL文を準備する
+			String sql = "UPDATE users SET icon_id=? WHERE user_id=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			if (icon_id != 0) {
+				pStmt.setInt(1, icon_id);
+			} else {
+				pStmt.setInt(1, 0);
+			}
+			if (user_id != 0) {
+				pStmt.setInt(2, user_id);
+			} else {
+				pStmt.setInt(2, 0);
+			}
+			
+
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
+    
+    
+    
 }
