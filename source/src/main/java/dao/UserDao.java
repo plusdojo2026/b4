eafe91cb44dcce0a5ccf0fe6e19cc81823a51ba5
plusdojo2026-nakatsukasa <nakatsukasa-kanako-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-25 15:17:02 +0900

    Update UserDao.java

--- source/src/main/java/dao/UserDao.java
+++ source/src/main/java/dao/UserDao.java
@@ -20,8 +20,13 @@ public class UserDao {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 
 				// データベースに接続する
-				conn = DriverManager.getConnection("jdbc:mysql ://localhost:3306/b4?useSSL= false&allowPublicKeyRetrieval=true&serverTimezone =Asia/Tokyo&connectTimeout =30000;",
-						"root", "password");
+				// サーバー環境
+				/* conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b4?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo&connectTimeout=30000",
+						"b4", "6vvRyvdGp4t4Cr3C");*/
+				// ローカル環境
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b4?"
+						+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+						"b4", "6vvRyvdGp4t4Cr3C"); 
 			
 				// SELECT文を準備する
 				String sql = "SELECT count(*) FROM users WHERE user_nickname=? AND password=?";
